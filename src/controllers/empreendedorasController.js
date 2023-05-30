81820a5b5524676ba0c2b2b3118566b30d1a7289
Vanessa Farias <vr.farias1992@gmail.com>
2023-05-30 11:56:36 -0300

    Atualização

--- src/controllers/empreendedorasController.js
+++ src/controllers/empreendedorasController.js
@@ -7,8 +7,9 @@ class EmpreendedoraController {
     });
   };
 
+
   static createEmpreendedoras = (req, res) => {
-    let empreendedora = new empreendedoras (req.body);
+    let empreendedora = new empreendedoras(req.body);
 
     empreendedora.save((err) => {
       if (err) {
@@ -18,8 +19,35 @@ class EmpreendedoraController {
       } else {
         res.status(201).send(empreendedora.toJSON());
       }
-    })
-  }
+    });
+  };
+
+
+  static updateEmpreendedoras = (req, res) => {
+    const id = req.params.id;
+    empreendedoras.findByIdAndUpdate(id, { $set: req.body }, (err) => {
+      if (!err) {
+        res
+          .status(200)
+          .send({ message: "Empreendedora atualizada com sucesso" });
+      } else {
+        res.status(500).send({ message: err.message });
+      }
+    });
+  };
+
+
+  static deleteEmpreendedoras = (req, res) => {
+    const id = req.params.id;
+    empreendedoras.findByIdAndDelete(id, (err) => {
+      if (!err) {
+        res.status(200).send({ message: "Empreendedora removida com sucesso" });
+      } else {
+        res.status(500).send({ message: err.message });
+      }
+    });
+  };
 }
+
 
 module.exports = EmpreendedoraController;
